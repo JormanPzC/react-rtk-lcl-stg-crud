90b6a8053a9deeb37b0f0c3a56758aa09cdfb272
Jorman Paz Castillo <jorman1323@gmail.com>
2022-10-03 18:55:50 -0400

    Update the routing of pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import TaskForm from "./components/TaskForm";
 import TasksList from "./components/TasksList";
 
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="bg-zinc-900 h-screen text-white">
       <div className="flex items-center justify-center h-full">
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<TasksList />} />
             <Route path="/create-task" element={<TaskForm />} />
             <Route path="/edit-task/:id" element={<TaskForm />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </div>
   );
